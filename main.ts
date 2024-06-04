6876baa7de4cc506ba9bf4add83119eea86279de
Basile Spaenlehauer <spaenleh@gmail.com>
2024-06-04 11:24:22 +0200

    fix: use website_configuration instead of bucket config for websiteEndpoint value (#27)

--- main.ts
+++ main.ts
@@ -442,7 +442,7 @@ class GraaspStack extends TerraformStack {
         this,
         `${id}-${website_name}`,
         website_name,
-        bucket.bucket.websiteEndpoint,
+        bucket.websiteConfiguration.websiteEndpoint,
         sslCertificateCloudfront,
         environment,
         !!bucket.websiteConfiguration,
