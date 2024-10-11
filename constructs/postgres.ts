eeb2d492c74a861b52d63b6f4b81d3d03d08aaa0
Basile Spaenlehauer <spaenleh@gmail.com>
2024-10-11 15:15:54 +0200

    fix(deps): update yarn version (#35)
    fix: update postgres version
    fix: update aws provider
    fix: update gatekeeper rule names

--- constructs/postgres.ts
+++ constructs/postgres.ts
@@ -57,7 +57,7 @@ export class PostgresDB extends Construct {
       identifier: `${name}`,
 
       engine: 'postgres',
-      engineVersion: '15.5',
+      engineVersion: '15.7',
       instanceClass: 'db.t3.micro',
       multiAz: false,
       availabilityZone: vpc.azs?.[0],
